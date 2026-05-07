45478d28431902140f1e05e3479abcbe72c683ae
technicalallcare-maker <technicalallcare@gmail.com>
2026-05-07 15:57:57 +0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,12 @@
 export const metadata = {
   title: 'All Care Technical Services',
   description: 'All Care Technical Services - Coming Soon',
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
-  );
+  )
 }
